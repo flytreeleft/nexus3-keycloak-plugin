e40c3006742240cf4b6d267d813148b663d55208
flytreeleft <flytreeleft@126.com>
2019-04-29 23:02:21 +0800

    Add three extended Keycloak authenticating realms

---
+++ src/main/java/org/github/flytreeleft/nexus3/keycloak/plugin/ext/auth1/KeycloakAuthenticatingRealm.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.github.flytreeleft.nexus3.keycloak.plugin.ext.auth1;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.eclipse.sisu.Description;
+import org.github.flytreeleft.nexus3.keycloak.plugin.internal.NexusKeycloakClientLoader;
+
+@Singleton
+@Named
+@Description("Keycloak Authentication Realm (#1)")
+public class KeycloakAuthenticatingRealm
+        extends org.github.flytreeleft.nexus3.keycloak.plugin.KeycloakAuthenticatingRealm {
+    public static final String NAME = KeycloakAuthenticatingRealm.class.getName();
+
+    public KeycloakAuthenticatingRealm() {
+        super(NexusKeycloakClientLoader.loadDefaultClient1());
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+}
