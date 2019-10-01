4a4e85f55e80fe98890617bee0c43927fdb63b34
flytreeleft <flytreeleft@126.com>
2019-10-01 11:57:45 +0800

    Develop: upgrade Nexus3 to version 3.16.2-01

--- develop/build-nexus3.sh
+++ develop/build-nexus3.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
-IMAGE_NAME=nexus3-keycloak-dev
-IMAGE_VERSION=3.6.0-02
 
+BASE_IMAGE_VERSION=3.16.2-01
+bash "${DIR}/cavemandaveman-nexus-docker/build.sh" ${BASE_IMAGE_VERSION}
+
+
+IMAGE_NAME=nexus3-keycloak-dev
+IMAGE_VERSION=${BASE_IMAGE_VERSION}
+
+echo "Build ${IMAGE_NAME}:${IMAGE_VERSION} ..."
 docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} \
              -f "${DIR}/Dockerfile" "${DIR}"
